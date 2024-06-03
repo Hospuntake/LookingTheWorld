d94ce959cd2201d3a2ba2189fd99e4e19ab9cf70
Hospuntake <joa2322002@gmail.com>
2024-06-03 21:48:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/hola")
+async def hola():
+    return {"message": "Hola"}
+
+@app.get("/adios")
+async def adios():
+    return {"message": "Adiós"}
+
+@app.get("/bienvenido")
+async def bienvenido():
+    return {"message": "Bienvenido"}
