5e1e98b34779a5bad7b4ff1675ed39927f7f803d
Hospuntake <joa2322002@gmail.com>
2024-06-07 19:43:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Configuración permitiendo solo un origen específico
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://example.com"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["X-Custom-Header"],
-)
 
 
 # Datos de las preguntas
@@ -31,40 +23,40 @@ data = [
         "Imagen": "https://i.ibb.co/Wg4znmB/imagen-bullying-marroqui.jpg",
         "NombreImagen": "imagen-bullying-marroqui.png",
         "frase": "The fight against racism begins with ___.",
-        "respuestaOK": "education",
-        "Respuestas": ["education", "ignore it", "been passive", "be racist"],
+        "respuestaOK": "Education",
+        "Respuestas": ["Education", "Ignore it", "Been passive", "Be racist"],
         "Reflexion": "Racism is a problem that we have had in our society for a long time. It occurs due to bad thinking from the education we received since we were children. We tend to be prejudiced against immigrants just because they are not similar to us or our culture."
     },
     {
         "Imagen": "https://i.ibb.co/h7Gx9b2/igualdaddegenero.jpg",
         "NombreImagen": "igualdaddegenero.jpg",
         "frase": "The fight for gender equality is a ___.",
-        "respuestaOK": "necessity",
-        "Respuestas": ["necessity", "nonsense", "charade", "not necessary"],
+        "respuestaOK": "Necessity",
+        "Respuestas": ["Necessity", "Nonsense", "Charade", "Not necessary"],
         "Reflexion": "The feminist movement that has occurred in recent decades focusing on equalizing the rights of women respect the rights of men has brought us closer to an era where we have an egalitarian society. Unfortunately, there is still a lot of effort needed to achieve this long-awaited equality; therefore, this feminist movement is still necessary."
     },
     {
         "Imagen": "https://i.ibb.co/TvhGvgj/respeto.jpg",
         "NombreImagen": "respeto.png",
         "frase": "We must teach future generations to ___ everyone equally.",
-        "respuestaOK": "respect",
-        "Respuestas": ["respect", "be edge with", "ignore", "look down on"],
+        "respuestaOK": "Respect",
+        "Respuestas": ["Respect", "Re edge with", "Rgnore", "Look down on"],
         "Reflexion": "Respect is very important to acquire from a young age. This quality is acquired thanks to a good education. If we respect everyone, the cases of different types of violence (gender, cultural...) would decrease drastically."
     },
     {
         "Imagen": "https://i.ibb.co/XLRR6Pf/localfood.jpg",
         "NombreImagen": "localfood.png",
         "frase": "It is important to consume ___ food.",
-        "respuestaOK": "local",
-        "Respuestas": ["local", "foreign", "fast food", "expired food"],
+        "respuestaOK": "Local",
+        "Respuestas": ["Local", "Foreign", "Fast food", "Expired food"],
         "Reflexion": "It is very important to consume local products since the process of those that are not pollutes the environment a lot, whether due to their transportation or manufacturing. It is also important to consume what you will actually consume because otherwise, there will be a lot of waste left. By buying in local markets you support the economy and the consumption of these products."
     },
     {
         "Imagen": "https://i.ibb.co/vQqNYhd/Las-3-rs.jpg",
         "NombreImagen": "Las-3-rs.png",
         "frase": "Which are the 3r?.",
-        "respuestaOK": "reuse, reduce, recycle",
-        "Respuestas": ["reuse, reduce, recycle", "reduce, restart, replay", "resume, recycle, reuse", "there's no 3r"],
+        "respuestaOK": "Reuse, Reduce, Recycle",
+        "Respuestas": ["Reuse, Reduce, Recycle", "Reduce, Restart, Replay", "Resume, Recycle, Reuse", "There's no 3r"],
         "Reflexion": "The 3 R's when it comes to recycling are reuse, reduce, and recycle. It is very important to pay attention to this simple rule since by doing so we can eliminate a large percentage of current pollution. It is a movement that is already present in our society, although there are still not enough measures. We have to take care of the world since it is not just ours."
     },
     {
@@ -88,7 +80,7 @@ data = [
         "NombreImagen": "pegar.png",
         "frase": "You see a man hit a woman in the middle of the street and there is no one else besides you. Do you also suspect that the man has a weapon, how should you act?",
         "respuestaOK": "Report the situation to the police through 016 or ask for help at 112 emergencies",
-        "Respuestas": ["Report the situation to the police through 016 or ask for help at 112 emergencies", "run away", "hit the man", "start screaming"],
+        "Respuestas": ["Report the situation to the police through 016 or ask for help at 112 emergencies", "Run away", "Hit the man", "Start screaming"],
         "Reflexion": "This situation is very complicated and at the same time dangerous, as it is a situation where there are no other people and you can suspect that the individual is carrying a weapon. It is best to call the authorities and have them take charge, always being careful to maintain a safe distance. Unfortunately, these events continue to occur in our society, and some unexpected day you"
     }
 ]
